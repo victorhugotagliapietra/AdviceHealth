1f07f13b2005de3eda2d9e3046186ad247e8c904
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-06-15 12:28:15 -0300

    refactor: vehicle routes

--- app/routes/vehicle_routes.py
+++ app/routes/vehicle_routes.py
@@ -2,42 +2,66 @@ from flask import Blueprint, request, jsonify
 from app import db
 from app.models.vehicle import Vehicle
 from app.models.client import Client
+from app.schemas.vehicle_schema import vehicle_schema, vehicles_schema
+from marshmallow import ValidationError
 
 bp = Blueprint('vehicle_routes', __name__, url_prefix='/vehicles')
 
 @bp.route('', methods=['POST'])
 def create_vehicle():
-  data = request.json
-  client = Client.query.get_or_404(data['client_id'])
-  if len(client.vehicles) >= 3:
-    return jsonify({'error': 'Client can have at most 3 vehicles'}), 400
-  new_vehicle = Vehicle(color=data['color'], model=data['model'], client_id=data['client_id'])
-  db.session.add(new_vehicle)
-  db.session.commit()
-  return jsonify({'id': new_vehicle.id}), 201
+  try:
+    data = request.json
+    vehicle_data = vehicle_schema.load(data, session=db.session)
+    client = Client.query.get_or_404(vehicle_data.client_id)
+    if len(client.vehicles) >= 3:
+      return jsonify({'error': 'Client can have at most 3 vehicles'}), 400
+    new_vehicle = Vehicle(
+      color=vehicle_data.color,
+      model=vehicle_data.model,
+      client_id=vehicle_data.client_id
+    )
+    db.session.add(new_vehicle)
+    db.session.commit()
+    return jsonify(vehicle_schema.dump(new_vehicle)), 201
+  except ValidationError as err:
+    return jsonify(err.messages), 400
+  except Exception as e:
+    db.session.rollback()
+    return jsonify({"error": "An error occurred while creating the vehicle.", "message": str(e)}), 500
 
 @bp.route('', methods=['GET'])
 def get_vehicles():
   vehicles = Vehicle.query.all()
-  return jsonify([{'id': vehicle.id, 'color': vehicle.color, 'model': vehicle.model, 'client_id': vehicle.client_id} for vehicle in vehicles])
+  return jsonify(vehicles_schema.dump(vehicles))
 
 @bp.route('/<uuid:vehicle_id>', methods=['GET'])
 def get_vehicle(vehicle_id):
   vehicle = Vehicle.query.get_or_404(vehicle_id)
-  return jsonify({'id': vehicle.id, 'color': vehicle.color, 'model': vehicle.model, 'client_id': vehicle.client_id})
+  return jsonify(vehicle_schema.dump(vehicle))
 
 @bp.route('/<uuid:vehicle_id>', methods=['PUT'])
 def update_vehicle(vehicle_id):
-  vehicle = Vehicle.query.get_or_404(vehicle_id)
-  data = request.json
-  vehicle.color = data.get('color', vehicle.color)
-  vehicle.model = data.get('model', vehicle.model)
-  db.session.commit()
-  return jsonify({'id': vehicle.id, 'color': vehicle.color, 'model': vehicle.model, 'client_id': vehicle.client_id})
+  try:
+    vehicle = Vehicle.query.get_or_404(vehicle_id)
+    data = request.json
+    vehicle_data = vehicle_schema.load(data, partial=True, session=db.session)
+    vehicle.color = vehicle_data.color
+    vehicle.model = vehicle_data.model
+    db.session.commit()
+    return jsonify(vehicle_schema.dump(vehicle))
+  except ValidationError as err:
+    return jsonify(err.messages), 400
+  except Exception as e:
+    db.session.rollback()
+    return jsonify({"error": "An error occurred while updating the vehicle.", "message": str(e)}), 500
 
 @bp.route('/<uuid:vehicle_id>', methods=['DELETE'])
 def delete_vehicle(vehicle_id):
-  vehicle = Vehicle.query.get_or_404(vehicle_id)
-  db.session.delete(vehicle)
-  db.session.commit()
-  return '', 204
+  try:
+    vehicle = Vehicle.query.get_or_404(vehicle_id)
+    db.session.delete(vehicle)
+    db.session.commit()
+    return '', 204
+  except Exception as e:
+    db.session.rollback()
+    return jsonify({"error": "An error occurred while deleting the vehicle.", "message": str(e)}), 500
