53a805bb11f26e34ec3a0a9031849cb71684a8e0
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-06-15 01:14:04 -0300

    feat: create init files

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from app.routes import client_routes, vehicle_routes
+    app.register_blueprint(client_routes.bp)
+    app.register_blueprint(vehicle_routes.bp)
+
+    return app
---
+++ app/models/__init__.py
@@ -0,0 +1,2 @@
+from .client import Client
+from .vehicle import Vehicle
