30b028db38c43230e2b85f40794b8f04cdf16667
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-06-16 03:22:12 -0300

    feat: add jwt secret key to app config

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ import os
 class Config:
   SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123@localhost:5432/postgres')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fallback_jwt_secret_key')
 
 class TestingConfig(Config):
   TESTING = True
   SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL', 'postgresql://postgres:123@localhost:5432/test_database')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fallback_jwt_secret_key_for_testing')
