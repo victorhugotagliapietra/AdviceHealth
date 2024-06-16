9fdf2a026a8f59cac5e42380e26ba2042d84b96f
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-06-16 00:43:51 -0300

    fix: update endpoint URL semantic

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -36,17 +36,13 @@ def test_create_vehicle(test_client):
 
   delete_client(client_id, test_client)
 
-def delete_client(client_id, test_client):
-  response = test_client.delete('/clients/'+client_id)
-  assert response.status_code == 204
-
 def test_get_client(test_client):
   client1 = test_client.post('/clients', json={'name': 'Test Client 1'})
   client2 = test_client.post('/clients', json={'name': 'Test Client 2'})
   client1_id = client1.json['id']
   client2_id = client2.json['id']
-  check_client1 = test_client.get('/clients/'+client1_id)
-  check_client2 = test_client.get('/clients/'+client2_id)
+  check_client1 = test_client.get(f'/clients/{client1_id}')
+  check_client2 = test_client.get(f'/clients/{client2_id}')
 
   assert check_client1.status_code == 200
   assert check_client1.json['name'] == 'Test Client 1'
@@ -55,4 +51,7 @@ def test_get_client(test_client):
 
   delete_client(client1_id, test_client)
   delete_client(client2_id, test_client)
-  
+
+def delete_client(client_id, test_client):
+  response = test_client.delete(f'/clients/{client_id}')
+  assert response.status_code == 204
