b4e72b044e52ca4ec65d34895937fe03929ca7ec
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-06-16 03:21:21 -0300

    feat: add auth routes blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app(config_class='config.Config'):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
+    jwt.init_app(app)
 
-    from app.routes import client_routes, vehicle_routes
+    from app.routes import client_routes, vehicle_routes, auth_routes
     app.register_blueprint(client_routes.bp)
     app.register_blueprint(vehicle_routes.bp)
+    app.register_blueprint(auth_routes.bp)
 
     return app
